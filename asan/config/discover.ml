ae1fd346b3780fc78a26765952fe5a2c10f87961
Bryan Phelps <bryphe@outrunlabs.com>
2020-02-22 13:19:16 -0500

    Initial commit: ASAN

---
+++ asan/config/discover.ml
@@ -0,0 +1,32 @@
+
+type os =
+    | Windows
+    | Mac
+    | Linux
+    | Unknown
+
+let uname () =
+    let ic = Unix.open_process_in "uname" in
+    let uname = input_line ic in
+    let () = close_in ic in
+    uname;;
+
+let get_os =
+    match Sys.os_type with
+    | "Win32" -> Windows
+    | _ -> match uname () with
+        | "Darwin" -> Mac
+        | "Linux" -> Linux
+        | _ -> Unknown
+
+let ccopt s = ["-ccopt"; s]
+let cclib s = ["-cclib"; s]
+
+
+let flags =
+    match get_os with
+    | Linux -> ccopt "-fsanitize=address"
+    | _ -> []
+;;
+
+Configurator.V1.Flags.write_sexp "flags.sexp" flags;
