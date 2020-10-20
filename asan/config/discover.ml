8d193f3d324a2bab710877e7ca5f4d7d9ff4e134
EduardoRFS <theeduardorfs@gmail.com>
2020-10-20 02:51:09 -0300

    config: implement better system detection

--- asan/config/discover.ml
+++ asan/config/discover.ml
@@ -1,32 +1,58 @@
+module Configurator = Configurator.V1
 
 type os =
-    | Windows
-    | Mac
-    | Linux
-    | Unknown
+  | Android
+  | IOS
+  | Linux
+  | Mac
+  | Windows
 
-let uname () =
-    let ic = Unix.open_process_in "uname" in
-    let uname = input_line ic in
-    let () = close_in ic in
-    uname;;
+let detect_system_header = {|
+  #if __APPLE__
+    #include <TargetConditionals.h>
+    #if TARGET_OS_IPHONE
+      #define PLATFORM_NAME "ios"
+    #else
+      #define PLATFORM_NAME "mac"
+    #endif
+  #elif __linux__
+    #if __ANDROID__
+      #define PLATFORM_NAME "android"
+    #else
+      #define PLATFORM_NAME "linux"
+    #endif
+  #elif WIN32
+    #define PLATFORM_NAME "windows"
+  #endif
+|}
 
-let get_os =
-    match Sys.os_type with
-    | "Win32" -> Windows
-    | _ -> match uname () with
-        | "Darwin" -> Mac
-        | "Linux" -> Linux
-        | _ -> Unknown
+let get_os t =
+  let header =
+    let file =
+      Filename.temp_file "discover" "os.h" in
+    let fd = open_out file in
+    output_string fd detect_system_header; close_out fd; file in
+  let platform =
+    Configurator.C_define.import
+      t
+      ~includes:[header]
+      [("PLATFORM_NAME", String)] in
+  match platform with
+  | (_, String "android") :: [] -> Android
+  | (_, String "ios") :: [] -> IOS
+  | (_, String "linux") :: [] -> Linux
+  | (_, String "mac") :: [] -> Mac
+  | (_, String "windows") :: [] -> Windows
+  | _ -> failwith "Unknown operating system"
 
 let ccopt s = ["-ccopt"; s]
 let cclib s = ["-cclib"; s]
 
-
-let flags =
-    match get_os with
-    | Linux -> ccopt "-fsanitize=address"
-    | _ -> []
-;;
-
-Configurator.V1.Flags.write_sexp "flags.sexp" flags;
+let () = Configurator.main
+  ~name:"reason-native-crash-utils"
+  (fun conf ->
+    let flags =
+      match get_os conf with
+      | Android | Linux -> ccopt "-fsanitize=address"
+      | _ -> [] in
+    Configurator.Flags.write_sexp "flags.sexp" flags)
